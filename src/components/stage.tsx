00ec3deabf6a441f1e8a19981410c59a55256a8e
cagdotin <13513097+cagdotin@users.noreply.github.com>
2024-12-31 17:41:02 +0300

    use hover-card for stage badge

--- src/components/stage.tsx
+++ src/components/stage.tsx
@@ -3,13 +3,8 @@ import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 import { Stage } from "@/lib/types";
 import { stageToRequisites } from "@/app/protocols/stageToRequisites";
-import { TooltipProvider } from "@radix-ui/react-tooltip";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "./rosette/tooltip/tooltip";
-
+import { HoverCard } from "./ui/hover-card";
+import { HoverCardContent, HoverCardTrigger } from "@radix-ui/react-hover-card";
 export interface BadgeProps {
   className?: string;
   stage: Stage;
@@ -17,40 +12,42 @@ export interface BadgeProps {
 
 export function StageBadge({ className, stage }: BadgeProps) {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger className="">
-          <Badge
-            variant="outline"
-            className={cn(
-              "border-none text-white rounded mx-auto bg-gray-500 hover:bg-gray-600 flex justify-center",
-              stage === 0 && "bg-red-500 hover:bg-red-600",
-              stage === 1 && "bg-yellow-500 hover:bg-yellow-600",
-              stage === 2 && "bg-green-500 hover:bg-green-600",
-              className
-            )}
-          >
-            {stage === "R" && "Review"}
-            {stage === "V" && "Variable"}
-            {typeof stage === "number" && `Stage ${stage}`}
-          </Badge>
-        </TooltipTrigger>
-        <TooltipContent fitContent className="border shadow-none z-50">
-          <div className="flex flex-col">
-            <span className="text-base font-bold">
-              <span className="mr-2">Stage of Decentralisation</span>
-            </span>
+    <HoverCard>
+      <HoverCardTrigger>
+        <Badge
+          variant="outline"
+          className={cn(
+            "border-none text-white rounded mx-auto bg-gray-500 hover:bg-gray-600 flex justify-center",
+            stage === 0 && "bg-red-500 hover:bg-red-600",
+            stage === 1 && "bg-yellow-500 hover:bg-yellow-600",
+            stage === 2 && "bg-green-500 hover:bg-green-600",
+            className
+          )}
+        >
+          {stage === "R" && "Review"}
+          {stage === "V" && "Variable"}
+          {typeof stage === "number" && `Stage ${stage}`}
+        </Badge>
+      </HoverCardTrigger>
+      <HoverCardContent
+        className="bg-background z-50 border p-4"
+        side="top"
+        sideOffset={4}
+      >
+        <div className="flex flex-col">
+          <span className="text-base font-bold">
+            <span className="mr-2">Stage of Decentralisation</span>
+          </span>
 
-            <div className="relative flex flex-col justify-center py-4 max-w-md font-mono">
-              {stageToRequisites[stage].map((item, index) => (
-                <div key={index} className="mt-1">
-                  {item}
-                </div>
-              ))}
-            </div>
+          <div className="relative flex flex-col justify-center py-4 max-w-md font-mono text-xs">
+            {stageToRequisites[stage].map((item, index) => (
+              <div key={index} className="mt-1">
+                {item}
+              </div>
+            ))}
           </div>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
   );
 }
